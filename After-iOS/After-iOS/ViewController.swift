0bf82caf0e65734367537226b32e749f08cb707f
heerucan <iwpow0307@gmail.com>
2022-01-05 03:16:09 +0900

    [Init] Beforeget Practice 프로젝트(#1)

---
+++ After-iOS/After-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  After-iOS
+//
+//  Created by Thisisme Hi on 2022/01/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
